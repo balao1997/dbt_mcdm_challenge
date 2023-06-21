6efb3849a145b72b4d2fe6b27abd1d5984c6277c
anastasiabalitzkaya@gmail.com <anastasiabalitzkaya@gmail.com>
2023-06-21 16:42:14 +0000

    bing

---
+++ models/marts/paid_ads__basic_performance.sql
@@ -0,0 +1,47 @@
+with bing as (
+    select 
+    date,
+    null as add_to_cart,
+    clicks, 
+    null as comments,
+    null as engagements,
+    impressions,
+    null as installs,
+    null as likes,
+    null as link_clicks,
+    null as post_click_conversions,
+    null as post_view_conversions,
+    null as posts,
+    null as purchase, 
+    null as registrations,
+    revenue,
+    null as shares,
+    spend, 
+    total_conversions,
+    null as video_views,
+    ad_id,
+    adset_id,
+    campaign_id,
+    channel, 
+    null as creative_id,
+    null as placement_id
+    from {{ ref('ads_bing')}}
+),
+
+facebook as (
+    select * from {{ ref('ads_facebook')}}
+),
+
+tiktok as (
+    select * from {{ ref('ads_tiktok')}}
+),
+
+twitter as (
+    select * from {{ ref('ads_twitter')}}
+),
+
+final as (
+    select * from bing
+)
+
+select * from final
