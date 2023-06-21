564e7d51e96b7cac65bbbd77bd4ff1e98b87aa77
anastasiabalitzkaya@gmail.com <anastasiabalitzkaya@gmail.com>
2023-06-21 11:34:11 +0000

    twitter

--- models/staging/ads_twitter.sql
+++ models/staging/ads_twitter.sql
@@ -1,1 +1,13 @@
-select * from {{ ref('src_promoted_tweets_twitter_all_data') }}
+select campaign_id,
+channel,
+clicks,
+comments,
+date,
+engagements,
+impressions,
+likes,
+url_clicks as link_clicks,
+retweets as shares,
+spend,
+video_total_views as video_views
+ from {{ ref('src_promoted_tweets_twitter_all_data') }}
