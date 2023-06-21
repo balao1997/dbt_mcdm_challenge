900d6c32deb32e722beaa3b0d77e45f5678d5452
anastasiabalitzkaya@gmail.com <anastasiabalitzkaya@gmail.com>
2023-06-21 11:23:15 +0000

    facebook

--- models/staging/ads_facebook.sql
+++ models/staging/ads_facebook.sql
@@ -1,1 +1,19 @@
-select * from {{ ref('src_ads_creative_facebook_all_data')}}
+select 
+    ad_id,
+    add_to_cart,
+    adset_id,
+    campaign_id,
+    channel,
+    clicks + clicks_2 as clicks,
+    comments + comments_2 as comments,
+    creative_id,
+    date,
+    impressions,
+    mobile_app_install as installs,
+    likes,
+    inline_link_clicks as link_clicks,
+    purchase + purchase_2 as purchase,
+    complete_registration as registrations,
+    shares + shares_2 as shares,
+    spend
+ from {{ ref('src_ads_creative_facebook_all_data')}}
