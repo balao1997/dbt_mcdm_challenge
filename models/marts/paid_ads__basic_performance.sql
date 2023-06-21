295d8d0ecbf15af18620807b5dc7c0794e59e889
anastasiabalitzkaya@gmail.com <anastasiabalitzkaya@gmail.com>
2023-06-21 17:25:34 +0000

    conv cost by channel

--- models/marts/paid_ads__basic_performance.sql
+++ models/marts/paid_ads__basic_performance.sql
@@ -89,15 +89,49 @@ tiktok as (
 ),
 
 twitter as (
-    select * from {{ ref('ads_twitter')}}
+    select 
+    date,
+    null as add_to_cart,
+    clicks, 
+    comments,
+    engagements,
+    impressions,
+    null as installs,
+    likes,
+    link_clicks,
+    null as post_click_conversions,
+    null as post_view_conversions,
+    null as posts,
+    null as purchase, 
+    null as registrations,
+    null as revenue,
+    shares,
+    spend, 
+    null as total_conversions,
+    video_views,
+    null as ad_id,
+    null as adset_id,
+    campaign_id,
+    channel, 
+    null as creative_id,
+    null as placement_id
+     from {{ ref('ads_twitter')}}
 ),
 
-final as (
+all_data as (
     select * from bing
     union all
     select * from facebook
     union all
     select * from tiktok
+    union all
+    select * from twitter
+),
+
+final as (
+    select channel, SUM(spend) / SUM(coalesce(total_conversions, purchase)) AS conversion_cost    
+    from all_data
+    group by channel
 )
 
 select * from final
